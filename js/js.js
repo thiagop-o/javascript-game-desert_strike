9618ff8e981e44008258765085159da3be96e49a
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-07 03:55:38 -0300

    Aula 14

--- js/js.js
+++ js/js.js
@@ -10,7 +10,7 @@ function start() { // Inicio da funcao start()
     //Principais variaveis do jogo
 
     var jogo = {}
-
+    var podeAtirar = true;
     var velocidade = 5;
     var posicaoY = parseInt(Math.random() * 334);
 
@@ -86,7 +86,7 @@ function start() { // Inicio da funcao start()
 
         if (jogo.pressionou[TECLA.D]) {
 
-            //Chama funcao Disparo	
+            disparo();
         }
 
     } // fim da funcao movejogador()
@@ -128,7 +128,39 @@ function start() { // Inicio da funcao start()
         }
 
     } // fim da funcao moveamigo()
+    
+    function disparo() {
 
+        if (podeAtirar == true) {
+
+            podeAtirar = false;
+
+            topo = parseInt($("#jogador").css("top"))
+            posicaoX = parseInt($("#jogador").css("left"))
+            tiroX = posicaoX + 190;
+            topoTiro = topo + 37;
+            $("#fundoGame").append("<div id='disparo'></div");
+            $("#disparo").css("top", topoTiro);
+            $("#disparo").css("left", tiroX);
+
+            var tempoDisparo = window.setInterval(executaDisparo, 30);
+
+        } //Fecha podeAtirar
+
+        function executaDisparo() {
+            posicaoX = parseInt($("#disparo").css("left"));
+            $("#disparo").css("left", posicaoX + 15);
+
+            if (posicaoX > 900) {
+
+                window.clearInterval(tempoDisparo);
+                tempoDisparo = null;
+                $("#disparo").remove();
+                podeAtirar = true;
+
+            }
+        } // Fecha executaDisparo()
+    } // Fecha disparo()
 
 
 } // Fim da funcao start
