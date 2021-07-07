19959718c3def7bc368ce42cfb810ef9b015dc53
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-07 03:39:15 -0300

    aula 11

--- js/js.js
+++ js/js.js
@@ -50,25 +50,39 @@ function start() { // Inicio da funcao start()
     } // fim da funcao movefundo()
 
     function movejogador() {
-	
+
         if (jogo.pressionou[TECLA.W]) {
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo-10);
-        
+            $("#jogador").css("top", topo - 10);
+
+            if (topo<=0) {
+		
+                $("#jogador").css("top",topo+10);
+            }
+            
+
+
         }
-        
+
         if (jogo.pressionou[TECLA.S]) {
-            
+
             var topo = parseInt($("#jogador").css("top"));
-            $("#jogador").css("top",topo+10);	
+            $("#jogador").css("top", topo + 10);
+
+            if (topo>=434) {	
+                $("#jogador").css("top",topo-10);
+                    
+            }
+
+
         }
-        
+
         if (jogo.pressionou[TECLA.D]) {
-            
+
             //Chama funcao Disparo	
         }
-    
-        } // fim da funcao movejogador()
+
+    } // fim da funcao movejogador()
 
 
 
