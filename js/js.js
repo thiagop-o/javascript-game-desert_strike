e8f388a194b1e23bd5ef98010df94a2ea80d8440
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-07 03:26:08 -0300

    Aula 8

---
+++ js/js.js
@@ -0,0 +1,33 @@
+function start() { // Inicio da funcao start()
+
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador' class='anima1' ></div>");
+    $("#fundoGame").append("<div id='inimigo1' class='anima2'></div>");
+    $("#fundoGame").append("<div id='inimigo2' ></div>");
+    $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
+
+    //Principais variaveis do jogo
+
+    var jogo = {}
+
+    //Game Loop
+
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+
+        movefundo();
+
+    } // Fim da funcao loop()
+
+    //Funcao que movimenta o fundo do jogo
+
+    function movefundo() {
+
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position", esquerda - 1);
+
+    } // fim da funcao movefundo()
+
+} // Fim da funcao start
