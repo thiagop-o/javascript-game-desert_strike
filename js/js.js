07a5ba4fc032fd4b64952d52779176d60cd9fcf0
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-07 03:46:05 -0300

    aula 13

--- js/js.js
+++ js/js.js
@@ -42,6 +42,8 @@ function start() { // Inicio da funcao start()
         movefundo();
         movejogador();
         moveinimigo1();
+        moveinimigo2();
+        moveamigo();
 
     } // Fim da funcao loop()
 
@@ -92,16 +94,40 @@ function start() { // Inicio da funcao start()
     function moveinimigo1() {
 
         posicaoX = parseInt($("#inimigo1").css("left"));
-        $("#inimigo1").css("left",posicaoX-velocidade);
-        $("#inimigo1").css("top",posicaoY);
-            
-            if (posicaoX<=0) {
+        $("#inimigo1").css("left", posicaoX - velocidade);
+        $("#inimigo1").css("top", posicaoY);
+
+        if (posicaoX <= 0) {
             posicaoY = parseInt(Math.random() * 334);
-            $("#inimigo1").css("left",694);
-            $("#inimigo1").css("top",posicaoY);
-                
-            }
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", posicaoY);
+
+        }
     } //Fim da funcao moveinimigo1()
+
+    function moveinimigo2() {
+        posicaoX = parseInt($("#inimigo2").css("left"));
+        $("#inimigo2").css("left", posicaoX - 3);
+
+        if (posicaoX <= 0) {
+
+            $("#inimigo2").css("left", 775);
+
+        }
+    } // Fim da funcao moveinimigo2()
+
+    function moveamigo() {
+
+        posicaoX = parseInt($("#amigo").css("left"));
+        $("#amigo").css("left", posicaoX + 1);
+
+        if (posicaoX > 906) {
+
+            $("#amigo").css("left", 0);
+
+        }
+
+    } // fim da funcao moveamigo()
 
 
 
