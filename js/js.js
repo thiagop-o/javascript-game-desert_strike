48675131e7303af95737e4e82652b5d726c0b47b
Thiago Oliveira <thiagop_o@hotmail.com>
2021-07-07 04:59:30 -0300

    Aula 23

--- js/js.js
+++ js/js.js
@@ -307,7 +307,26 @@ function start() { // Inicio da funcao start()
         if (energiaAtual == 0) {
             $("#energia").css("background-image", "url(imgs/energia0.png)");
             //Game Over
+            gameOver();
         }
     } // Fim da funcao energia()
+    //Funcao GAME OVER
+	function gameOver() {
+        fimdejogo=true;
+        musica.pause();
+        somGameover.play();
+        
+        window.clearInterval(jogo.timer);
+        jogo.timer=null;
+        
+        $("#jogador").remove();
+        $("#inimigo1").remove();
+        $("#inimigo2").remove();
+        $("#amigo").remove();
+        
+        $("#fundoGame").append("<div id='fim'></div>");
+        
+        $("#fim").html("<h1> Game Over </h1><p>Sua pontuação foi: " + pontos + "</p>" + "<div id='reinicia' onClick=reiniciaJogo()><h3>Jogar Novamente</h3></div>");
+        } // Fim da funcao gameOver();
 
 } // Fim da funcao start
